254673985da60d7f85b7242c81c2417c0fb1223c
KTH_NUC <kthzstn@gmail.com>
2022-03-03 03:37:37 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -1,3 +1,31 @@
+
+var galleryPhotos1 = [
+	"images/sample_images_01.png",
+	"images/sample_images_02.png",
+	"images/sample_images_03.png",
+	"images/sample_images_04.png",
+	"images/sample_images_05.png",
+	"images/sample_images_06.png",
+	"images/sample_images_07.png",
+	"images/sample_images_08.png",
+	"images/sample_images_09.png",
+	"images/sample_images_10.png",
+	"images/sample_images_11.png",
+	"images/sample_images_12.png"
+];
+
+var galleryPhotos2 = [
+	"images/sample_images_01.png",
+	"images/sample_images_02.png",
+	"images/sample_images_03.png",
+	"images/sample_images_04.png",
+	"images/sample_images_05.png",
+	"images/sample_images_06.png",
+	"images/sample_images_07.png",
+	"images/sample_images_08.png",
+	"images/sample_images_09.png"
+]
+
 $(document).on("ready", function(){
     $("body").append("<div id='menu-overlay'></div>");
 
@@ -115,6 +143,26 @@ function dataPicker(data) { // 달력 출력
 var imgGallerySlider;
 var popGallerySlider;
 function imageGallery() { // 포토갤러리
+	for(var i = 0; i < galleryPhotos1.length; i++){
+		$('#image-gallery').append(
+			"<li data-thumb=\""+galleryPhotos1[i]+"\" onclick=\"galleryPOP('open', 'type1');\">" 
+					+"<div class=\"box\">"
+					  +"<div class=\"pos\">"
+						+"<div class=\"cen\">"
+						  +"<img src=\""+galleryPhotos1[i]+"\" />"
+						+"</div>"
+					  +"</div>"
+					+"</div>"
+				  +"</li>"
+			);
+	}
+	
+	for(var i = 0; i < galleryPhotos2.length; i++){
+		$('#grid-gallery').append(
+			"<figure><a href=\"javascript:galleryPOP('open', 'type2', "+(i+1)+");\" style=\"background: url('"+galleryPhotos2[i]+"' ) no-repeat center center\" itemprop=\"contentUrl\" data-size=\"800x1200\" class=\"setimgsize\"><img src=\"./images/img_frame.png\" class=\"img_frame\" itemprop=\"thumbnail\" alt=\"\"></a> </figure>"
+		);
+	}
+
 	imgGallerySlider = $('#image-gallery').lightSlider({
 		gallery:true,
 		item:1,
@@ -154,6 +202,28 @@ function popGallery() { // 팝업갤러리
 }
 function galleryPOP(toggle, type, index) { // 갤러리팝업 열고,닫기
 	if(type === "type1") return galleryType1(toggle);
+	else if(type === "type2") return galleryType2(toggle, index);
+}
+function popGalleryImageAppend(photos){
+	$("#pop-body").append("<ul id=\"pop-gallery\" class=\"gallery list-unstyled cS-hidden roll_type01\"></ul>")
+	for (var i = 0; i < photos.length; i++){
+		$("#pop-gallery").append(
+			"<li data-thumb=\""+photos[i]+"\">"
+			+"<div class=\"box\">"
+				+"<div class=\"pos\">"
+					+"<div class=\"cen\">"
+						+"<img src=\""+photos[i]+"\" />"
+					+"</div>"
+				+"</div>"
+			+"</div>"
+		+"</li>"
+		);
+	}
+	popGallery();
+}
+function popGalleryImageClear(){
+	$("#pop-body").empty();
+	popGallerySlider = null;
 }
 function galleryType1(toggle){ // 슬라이드형 갤러리
 	var el = $(".gallery-pop-wrap"),
@@ -162,6 +232,7 @@ function galleryType1(toggle){ // 슬라이드형 갤러리
 	if(sw.hasClass('moveOn')) return this;
 	
 	if(toggle === "open"){
+		popGalleryImageAppend(galleryPhotos1);
 		el.css({
 			"opacity": 1,
 			"transform": "translate3d(0,0,0)"
@@ -177,6 +248,35 @@ function galleryType1(toggle){ // 슬라이드형 갤러리
 		imgGallerySlider.goToSlide(index);
 		el.removeAttr("style");
 		el.find(".pop-body").removeAttr("style");
+		popGalleryImageClear();
+		scrollAuto();
+	}
+}
+function galleryType2(toggle, index){ // 슬라이드형 갤러리
+	var el = $(".gallery-pop-wrap"),
+		sw = $(".lSSlideWrapper");
+
+	if(sw.hasClass('moveOn')) return this;
+	
+	if(toggle === "open"){
+		popGalleryImageAppend(galleryPhotos2);
+		el.css({
+			"opacity": 1,
+			"transform": "translate3d(0,0,0)"
+		});
+		el.find(".pop-body").css({
+			"visibility": "visible"
+		}).delay(500).animate({
+			"opacity" : 1
+		}, 300);
+		popGallerySlider.goToSlide(index);
+		scrollFixed();
+	} else if(toggle === "close"){
+		var index = $('#pop-gallery li.active').index();
+		imgGallerySlider.goToSlide(index);
+		el.removeAttr("style");
+		el.find(".pop-body").removeAttr("style");
+		popGalleryImageClear();
 		scrollAuto();
 	}
 }
